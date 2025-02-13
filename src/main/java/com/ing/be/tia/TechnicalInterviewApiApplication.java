9c213a23edf19a12d75bb1c4df2faf4425cf6ea1
Olie Santos <olie.santos2@ing.com>
2025-02-13 15:33:34 +0100

    INITIAL COMMIT

---
+++ src/main/java/com/ing/be/tia/TechnicalInterviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.ing.be.tia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalInterviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalInterviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/be/tia/TechnicalInterviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.be.tia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalInterviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
